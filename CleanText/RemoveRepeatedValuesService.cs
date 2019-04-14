3552462df6b9eb6076b69a043fcc77cb2645e086
Wendell Antildes <wendellantildes@gmail.com>
2019-04-14 20:58:18 -0300

    First commit with method that removes repeated values. Tests.

---
+++ CleanText/RemoveRepeatedValuesService.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace CleanText
+{
+    public sealed class RemoveRepeatedValuesService
+    {
+        /// <summary>
+        /// Split a string and remove all repeated values
+        /// </summary>
+        /// <returns>The string without repeated values.</returns>
+        /// <param name="text">A text.</param>
+        /// <param name="separator">A separator.</param>
+        public static string FromString(string text, char separator)
+        {
+            if(text == null)
+            {
+                return text;
+            }
+            var words = text.Split(separator);
+            return words.Distinct().Aggregate((i,j) => $"{i}{separator}{j}");
+        }
+    }
+}
---
+++ Tests/RemoveRepeatedValuesServiceTests.cs
@@ -0,0 +1,41 @@
+using System;
+using CleanText;
+using Xunit;
+using System.Linq;
+
+namespace Tests
+{
+    public class RemoveRepeatedValuesServiceTests
+    {
+        private const string Stage = "stage";
+        private const string Development = "development";
+        private const string Production = "production";
+
+        [Theory(DisplayName = "Repeated Values")]
+        [InlineData("stage;development;production")]
+        [InlineData("stage;stage;development;development;production;production")]
+        [InlineData("stage;stage;stage;development;development;development;production;production;production")]
+        public void RemoveRepeatedValuesService_FromString_GetStringWithoutRepeatedValues(string envNames)
+        {
+            var envNamesWithoutRepeatedValues = RemoveRepeatedValuesService.FromString(envNames, ';');
+            var words = envNamesWithoutRepeatedValues.Split(';');
+            Assert.Equal(1, words.Count(x => x == Stage));
+            Assert.Equal(1, words.Count(x => x == Development));
+            Assert.Equal(1, words.Count(x => x == Production));
+        }
+
+        [Fact]
+        public void RemoveRepeatedValuesService_FromString_GetNullString()
+        {
+            var envNamesWithoutRepeatedValues = RemoveRepeatedValuesService.FromString(null, ';');
+            Assert.Null(envNamesWithoutRepeatedValues);
+        }
+
+        [Fact]
+        public void RemoveRepeatedValuesService_FromString_GetEmptyString()
+        {
+            var envNamesWithoutRepeatedValues = RemoveRepeatedValuesService.FromString(string.Empty, ';');
+            Assert.True(string.Empty == envNamesWithoutRepeatedValues);
+        }
+    }
+}
